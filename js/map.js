94ed1eb4bef8535f4726b49c92ad6cc3f8c9e118
mathieu-neyret <mathieu.n@biosample.fr>
2021-03-18 15:17:48 +0100

    Ajout des liens/menus du site

--- js/map.js
+++ js/map.js
@@ -15,17 +15,3 @@
     L.marker([45.766211, 4.792767]).addTo(map)
     .bindPopup('David Guetta', {closeOnClick: false, autoClose: false})
     .openPopup();
-
-/*var map = L.map('map').setView([51.505, -0.09], 13);
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
-        }).addTo(map);
-
-        var marker = L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.', {closeOnClick: false, autoClose: false})
-    .openPopup();
-
-    var l2 =L.marker([51.48, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.', {closeOnClick: false, autoClose: false})
-    .openPopup();
-   */
